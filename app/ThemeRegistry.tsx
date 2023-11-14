4d1205dc2fba212ac40c0e284de93dd0224d5652
Shaon <shaon.softbdltd@gmail.com>
2023-11-14 18:33:43 +0600

    added mui theme

---
+++ app/ThemeRegistry.tsx
@@ -0,0 +1,64 @@
+'use client';
+
+import {ReactNode, useState} from 'react';
+import createCache from '@emotion/cache';
+import {useServerInsertedHTML} from 'next/navigation';
+import {CacheProvider} from '@emotion/react/macro';
+import {CssBaseline, ThemeProvider} from '@mui/material';
+import theme from '@/layouts/themes';
+
+type ThemeRegistry = {
+  options: {key: string};
+  children: ReactNode;
+};
+
+export default function ThemeRegistry({options, children}: ThemeRegistry) {
+  const [{cache, flush}] = useState(() => {
+    const cache = createCache(options);
+    cache.compat = true;
+    const prevInsert = cache.insert;
+    let inserted: string[] = [];
+    cache.insert = (...args) => {
+      const serialized = args[1];
+      if (cache.inserted[serialized.name] === undefined) {
+        inserted.push(serialized.name);
+      }
+      return prevInsert(...args);
+    };
+    const flush = () => {
+      const prevInserted = inserted;
+      inserted = [];
+      return prevInserted;
+    };
+    return {cache, flush};
+  });
+
+  useServerInsertedHTML(() => {
+    const names = flush();
+    if (names.length === 0) {
+      return null;
+    }
+    let styles = '';
+    for (const name of names) {
+      styles += cache.inserted[name];
+    }
+    return (
+      <style
+        key={cache.key}
+        data-emotion={`${cache.key} ${names.join(' ')}`}
+        dangerouslySetInnerHTML={{
+          __html: styles,
+        }}
+      />
+    );
+  });
+
+  return (
+    <CacheProvider value={cache}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        {children}
+      </ThemeProvider>
+    </CacheProvider>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
+import React from 'react';
 import type {Metadata} from 'next';
 import {Inter} from 'next/font/google';
 import './globals.css';
+import ThemeRegistry from '@/app/ThemeRegistry';
 
 const inter = Inter({subsets: ['latin']});
 
@@ -12,7 +14,9 @@ export const metadata: Metadata = {
 export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
     <html lang='en'>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ThemeRegistry options={{key: 'mui'}}>{children}</ThemeRegistry>
+      </body>
     </html>
   );
 }
