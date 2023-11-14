d73da3ddec5b551439bb8c511c210e5b571bd085
shakib <shakib.shaon@softbdltd.com>
2023-11-14 22:38:11 +0600

    fix: mui theme issue

--- app/ThemeRegistry.tsx
+++ app/ThemeRegistry.tsx
@@ -3,21 +3,23 @@
 import {ReactNode, useState} from 'react';
 import createCache from '@emotion/cache';
 import {useServerInsertedHTML} from 'next/navigation';
-import {CacheProvider} from '@emotion/react/macro';
 import {CssBaseline, ThemeProvider} from '@mui/material';
 import theme from '@/layouts/themes';
+import {CacheProvider} from '@emotion/react';
 
-type ThemeRegistry = {
+type ThemeRegistryProps = {
   options: {key: string};
   children: ReactNode;
 };
 
-export default function ThemeRegistry({options, children}: ThemeRegistry) {
+export default function ThemeRegistry({options, children}: ThemeRegistryProps) {
   const [{cache, flush}] = useState(() => {
     const cache = createCache(options);
     cache.compat = true;
+
     const prevInsert = cache.insert;
     let inserted: string[] = [];
+
     cache.insert = (...args) => {
       const serialized = args[1];
       if (cache.inserted[serialized.name] === undefined) {
@@ -25,23 +27,25 @@ export default function ThemeRegistry({options, children}: ThemeRegistry) {
       }
       return prevInsert(...args);
     };
+
     const flush = () => {
       const prevInserted = inserted;
       inserted = [];
       return prevInserted;
     };
+
     return {cache, flush};
   });
 
   useServerInsertedHTML(() => {
     const names = flush();
+
     if (names.length === 0) {
       return null;
     }
-    let styles = '';
-    for (const name of names) {
-      styles += cache.inserted[name];
-    }
+
+    const styles = names.map((name) => cache.inserted[name]).join('');
+
     return (
       <style
         key={cache.key}
